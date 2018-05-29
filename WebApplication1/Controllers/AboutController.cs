87be5a8c402cfd67da77bda1dfb7fa9bd782c184
Uberacy Tottoli <bera@boldtech.com.br>
2018-05-29 06:32:52 -0300

    add controllers

---
+++ WebApplication1/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+	[Route("[controller]/[action]")]
+    public class AboutController
+    {
+		public string Address() => "USA";
+		public string Phone()
+	    {
+		    return "335484";
+	    }
+	}
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,35 +17,32 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-	        services.AddSingleton<IGreeting, Greeter>();
+	        services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration config, IGreeting greeter)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration config)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			if (env.IsDevelopment())
+			{
+				//Middleware
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync(greeter.GetMessage());
-            });
-        }
-    }
+	        app.UseStaticFiles();
 
-	public interface IGreeting
-	{
-		string GetMessage();
-	}
+	        app.UseMvc(ConfigureRoutes);
 
-	public class Greeter :IGreeting
-	{
-		public string GetMessage()
-		{
-			return "TESTE";
+			//Simple middleware
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Not Found");
+			});
 		}
-	}
 
+	    private void ConfigureRoutes(IRouteBuilder routeBuilder)
+	    {
+		    routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+	    }
+    }
 }
